5b3e14459b72c7223bc4ade5845df7855fbd8fb3
ldettorre <ldettorre21@gmail.com>
2019-08-25 23:56:09 +0100

    Added logic to stop the same card being clicked twice in a turn and also a shuffle feature to randomize the cards each time the page is refreshed.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,9 +4,13 @@ var cards = document.querySelectorAll('.card');
 
 let flippedCard = false;
 let firstCard, secondCard;
+let boardLocked = false
 
 // Using 'classList' allows easy access to an elements list of classes. 'toggle' then switches when the desired class is applied and removed.
 function flipTheCard(){
+    if (boardLocked) return;
+    if (this === firstCard) return;
+
     this.classList.toggle('flipped');
     // This is the players first choice  
     if (!flippedCard){
@@ -14,7 +18,6 @@ function flipTheCard(){
         firstCard = this;   //'this' is the card that triggered the event 
     } else {
         // This is the players second choice
-        flippedCard = false;
         secondCard = this;
         // If they match, we will remove the event listener that would flip them back to default position.
         
@@ -33,15 +36,31 @@ function checkIFMatching(){
 function disableCards(){
     firstCard.removeEventListener('click',flipTheCard);
     secondCard.removeEventListener('click',flipTheCard);
+    boardReset();
 }
 
 //Should the cards not match, they will return to the default position.
 function resetCards(){
+    boardLocked = true; 
     setTimeout(() => {
         firstCard.classList.toggle('flipped');
         secondCard.classList.toggle('flipped');
+        boardReset();
         }, 1000);
 }
+
+function boardReset(){
+    [flippedCard,boardLocked] = [false,false];
+    [firstCard,secondCard] = [null,null];
+}
+
+// Below generates a random number and assigns it to each card. We use math.random to generate the random number and math.floor to create an integer.
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPosition = Math.floor(Math.random()*12);
+        card.style.order = randomPosition;
+    });
+})();
 // An event listener has been added to each card (click) and it executes a function when performed (flipTheCard)
 cards.forEach(card => card.addEventListener('click', flipTheCard));
 
